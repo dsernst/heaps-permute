bf60442c4112b2732510f1c44eae5366a9ee1c90
David Ernst <git@dsernst.com>
2015-09-03 20:55:00 -0700

    Add unit tests

--- test.js
+++ test.js
@@ -1,5 +1,55 @@
-var permute = require('./index.js');
+/*global describe,it*/
 
-permute(['a', 'b', 'c', 'd'], console.log);
-permute([1, 2, 3], console.log);
+var permute = require('./index.js');
+var assert = require('assert');
+
+describe('heaps-permute', function () {
+  it('returns all permutations of an array of items', function () {
+    var input = [1, 2, 3];
+    var expected = [
+      [ 1, 2, 3 ],
+      [ 2, 1, 3 ],
+      [ 3, 1, 2 ],
+      [ 1, 3, 2 ],
+      [ 2, 3, 1 ],
+      [ 3, 2, 1 ]
+    ];
+
+    assert.deepEqual(permute(input), expected);
+  });
+
+  it('can permute strings too', function () {
+    var input = ['a', 'b', 'c', 'd'];
+    var expected = [
+      [ 'a', 'b', 'c', 'd' ],
+      [ 'b', 'a', 'c', 'd' ],
+      [ 'c', 'a', 'b', 'd' ],
+      [ 'a', 'c', 'b', 'd' ],
+      [ 'b', 'c', 'a', 'd' ],
+      [ 'c', 'b', 'a', 'd' ],
+      [ 'd', 'b', 'c', 'a' ],
+      [ 'b', 'd', 'c', 'a' ],
+      [ 'c', 'd', 'b', 'a' ],
+      [ 'd', 'c', 'b', 'a' ],
+      [ 'b', 'c', 'd', 'a' ],
+      [ 'c', 'b', 'd', 'a' ],
+      [ 'd', 'a', 'c', 'b' ],
+      [ 'a', 'd', 'c', 'b' ],
+      [ 'c', 'd', 'a', 'b' ],
+      [ 'd', 'c', 'a', 'b' ],
+      [ 'a', 'c', 'd', 'b' ],
+      [ 'c', 'a', 'd', 'b' ],
+      [ 'd', 'a', 'b', 'c' ],
+      [ 'a', 'd', 'b', 'c' ],
+      [ 'b', 'd', 'a', 'c' ],
+      [ 'd', 'b', 'a', 'c' ],
+      [ 'a', 'b', 'd', 'c' ],
+      [ 'b', 'a', 'd', 'c' ]
+    ];
+
+    assert.deepEqual(permute(input), expected);
+
+  });
+
+});
 
