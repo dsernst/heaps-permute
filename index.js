a9bbdc03a5eb27c184a43fc21ecae4f4a180fce1
David Ernst <git@dsernst.com>
2015-09-03 20:30:04 -0700

    Properly export function and cleanup code

--- index.js
+++ index.js
@@ -1,31 +1,25 @@
-var swap = function (array, pos1, pos2) {
+function swap(array, pos1, pos2) {
   var temp = array[pos1];
   array[pos1] = array[pos2];
   array[pos2] = temp;
-};
-
-var heapsPermute = function (array, output, n) {
-  n = n || array.length; // set n default to array.length
-  if (n === 1) {
-    output(array);
-  } else {
-    for (var i = 1; i <= n; i += 1) {
-      heapsPermute(array, output, n - 1);
-      if (n % 2) {
-        var j = 1;
-      } else {
-        var j = i;
-      }
-      swap(array, j - 1, n - 1); // -1 to account for javascript zero-indexing
-    }
-  }
-};
-
-
-// For testing:
-var print = function(input){
-  console.log(intput);
 }
 
-heapsPermute(['a', 'b', 'c', 'd'], print);
-// heapsPermute([1, 2, 3], print)
+function heapsPermute(array, callback, n) {
+  n = n || array.length; // set n default to array.length
+  if (n === 1) {
+    callback(array);
+  } else {
+    var i, j;
+    for (i = 1; i <= n; i += 1) {
+      heapsPermute(array, callback, n - 1);
+      if (n % 2) {
+        j = 1;
+      } else {
+        j = i;
+      }
+      swap(array, j - 1, n - 1); // -1 accounts for zero-indexing
+    }
+  }
+}
+
+module.exports = heapsPermute;
